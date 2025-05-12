70ad10797f164601d1a7bef3156f5c70aa4dbd14
Nandhana <nandhanarajeev03@gmail.com>
2025-05-12 17:17:33 +0530

    fixing ?

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const authenticateUser = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Extract token from Authorization header
+  const token = req.headers.authorization.split(" ")[1]; // Extract token from Authorization header
 
   if (!token) {
     return res.status(401).json({ error: "No token provided" });
