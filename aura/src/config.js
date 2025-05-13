da466a8b4295ca9e00d524eaf38e5db498f8dd61
Nandhana <nandhanarajeev03@gmail.com>
2025-05-13 09:48:08 +0530

    pushing build

--- aura/src/config.js
+++ aura/src/config.js
@@ -1,5 +1,5 @@
 // config.js
 // const SERVER_URL = 'http://localhost:3000';
-const SERVER_URL = 'http://20.118.232.175:81';
+const SERVER_URL = 'http://20.118.232.175';
 
 export default SERVER_URL;
